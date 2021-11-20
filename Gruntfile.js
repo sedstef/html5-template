d872c5bc83b4681c3f15a51b952859307d5db0eb
Stefan Sedelmaier <stefan@sedelmaier.at>
2021-11-20 16:18:10 +0100

    add grunt-run

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,27 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         htmllint: {
+            //see https://www.npmjs.com/package/grunt-html
             all: ['src/**/*.html']
+        },
+        run: {
+            //see https://www.npmjs.com/package/grunt-run
+            httpd: {
+                cmd: './start-httpd.sh',
+//                args: [
+//                    'arg1',
+//                    'arg2'
+//                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-run');
+
 
     grunt.registerTask('default', ['htmllint']);
+
+    grunt.registerTask('build', ['htmllint']);
 
 };
