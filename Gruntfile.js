3434d4f88136d76bbb02d1af012b1f9f76cb8d12
Stefan Sedelmaier <stefan@sedelmaier.at>
2021-11-20 16:59:40 +0100

    add csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,24 @@ module.exports = function (grunt) {
             //see https://www.npmjs.com/package/grunt-html
             all: ['src/**/*.html']
         },
+        csslint: {
+            //see https://github.com/gruntjs/grunt-contrib-csslint
+            options: {
+                csslintrc: '.csslintrc'
+            },
+            strict: {
+                options: {
+                    import: 2
+                },
+                src: ['src/**/*.css']
+            },
+            lax: {
+                options: {
+                    import: false
+                },
+                src: ['src/**/*.css']
+            }
+        },
         run: {
             //see https://www.npmjs.com/package/grunt-run
             httpd: {
@@ -19,11 +37,12 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-run');
 
 
-    grunt.registerTask('default', ['htmllint']);
+    grunt.registerTask('default', ['htmllint', 'csslint']);
 
     grunt.registerTask('build', ['htmllint']);
-    
+
 };
