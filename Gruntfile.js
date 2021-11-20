224b75ffbe573c840c85d20272b34a1efe2ff626
Stefan Sedelmaier <stefan@sedelmaier.at>
2021-11-20 15:36:08 +0100

    add htmllinter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,14 @@
-
 module.exports = function (grunt) {
-    // Project configuration.
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
+        htmllint: {
+            all: ['src/**/*.html']
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-html');
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['htmllint']);
+
 };
